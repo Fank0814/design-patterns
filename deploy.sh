b919320c80c502a98ab344d1d61b8d7bb87efbfb
Yuanxin Dong <yuanxin.me@gmail.com>
2019-01-22 21:38:48 +0800

    add: 更新脚本

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,32 @@
 #!/usr/bin/env sh
 
+https="https://github.com/dongyuanxin/design-patterns.git"
+git="git@github.com:dongyuanxin/design-patterns.git"
+
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
-vuepress build .
+# vuepress build .
 
 # 进入生成的文件夹
-cd .vuepress/dist/
+# cd .vuepress/dist/
 
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:dongyuanxin/design-patterns.git master:gh-pages
+# git init
+# git add -A
+# git commit -m 'deploy'
 
-cd -
+echo $1;
+
+if [ $1 == "https" ]
+then
+  git push -f ${https} master:gh-pages
+else if [ $1 == "ssh" ]
+  git push -f ${ssh} master:gh-pages
+then
+  git push -f git@github.com:dongyuanxin/design-patterns.git master:gh-pages
+fi
+
+# git push -f https://github.com/dongyuanxin/design-patterns.git master:gh-pages
+
+# cd -
