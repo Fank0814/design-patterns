6e6b6a0b1d6061b4620242eeea96a920bcf9d992
Yuanxin Dong <yuanxin.me@gmail.com>
2019-01-29 16:10:57 +0800

    change: 桥接模式js代码

--- bridge-pattern/main.js
+++ bridge-pattern/main.js
@@ -1,5 +1,9 @@
+// 抽象部分
 const forEach = (arr, callback) => {
-  if (!Array.isArray(arr)) return;
+  if (
+    !Array.isArray(arr) ||
+    typeof callback !== 'function'
+  ) return;
 
   const length = arr.length;
   for (let i = 0; i < length; ++i) {
@@ -7,6 +11,7 @@ const forEach = (arr, callback) => {
   }
 };
 
-// 以下是测试代码
+/**************以下是测试代码**************/
 let arr = ["a", "b"];
+// 回调函数是具体实现部分
 forEach(arr, (el, index) => console.log("元素是", el, "位于", index));
